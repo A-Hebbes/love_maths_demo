fb314dbf4b43ac32e682eb59e379d79938797e6d
Alex Hebbes <aeh7@live.co.uk>
2024-01-01 07:57:54 +0000

    feat: added addition game operands and operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ for (let button of buttons){
             alert ("You clicked Submit");
         } else {
             let gameType = this.getAttribute ("data-type");
-            alert (`You clicked ${gameType}`);
+            runGame(gameType);
         }
     });
 }
+
+runGame("addition");
 
 });
 
@@ -19,48 +21,75 @@ for (let button of buttons){
  * the user's answer has been processed
  */
 
-function runGame()(
+function runGame(gameType){
     let num1 = Math.floor(Math.random()*25) + 1;
     let num2 = Math.floor(Math.random()*25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
 
-)
+    }
+
+
+}
+    
 
 
 
-function checkAnswer()(
+
+
+function checkAnswer(){
+
+}
     
     
-    )
+    
 
-function calculateCorrectAnswer()(
+function calculateCorrectAnswer(){
+
+}
 
     
-        )
+        
 
 
-function incrementScore()(
+function incrementScore(){
 
-    
-)
-
-function incrementWrongAnswer()(
+}
 
     
-    )
-
-function displayAdditionQuestion()(
-
-    
-        )
 
 
-function displaySubtractQuestion()(
+function incrementWrongAnswer(){
+
+}
 
     
-            )
+    
 
-function displayMultiplyQuestion () (
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById ('operand1') .textContent = operand1;
+document.getElementById ('operand2') .textContent = operand2;
+document.getElementById ('operator') .textContent = "+";
+
+}
+
+    
+        
 
 
-)
+function displaySubtractQuestion(){
+
+}
+
+    
+            
+
+function displayMultiplyQuestion () {
+
+}
+
+
