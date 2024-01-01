843ba6d8f0e4fb56557e17cb61962d94fd9d6614
Alex Hebbes <aeh7@live.co.uk>
2024-01-01 07:36:16 +0000

    feat: added a function to produce randome numbers for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,22 @@ for (let button of buttons){
 
 });
 
-function rungame()(
+/**
+ * The main game "loop", called when the script is first loaded and after the 
+ * the user's answer has been processed
+ */
+
+function runGame()(
+    let num1 = Math.floor(Math.random()*25) + 1;
+    let num2 = Math.floor(Math.random()*25) + 1;
 
 
 )
 
-function checkAnswer()(
 
+
+function checkAnswer()(
+    
     
     )
 
